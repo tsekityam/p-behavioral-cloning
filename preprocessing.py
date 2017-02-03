fe230ae66ec04fa7d464c5319dae8cea8f3fd24c
Tse Kit Yam <me@kytse.com>
2017-02-03 16:57:46 +0800

    Remove debug log

--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,6 @@ def get_cropped_images(images):
     shape = images[0].shape
     height = shape[0]
     width = shape[1]
-    print(images.shape)
     return images[0:len(images), 40:height-20, 0:width]
 
 def preprocess_input(images):
